f76a067ec0b98ba36a699cffd4c43a86855b6731
EdsonEstevan <edsonestevank@gmail.com>
2023-05-25 16:50:27 -0300

    recedu

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,221 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { livros } from '@/_data/livros.js'
+
 const carrinho = ref({
   itens: [],
   total: 0
 })
 
-function atualizaQuantidadeItem(item) {
+const adicionarAoCarrinho = (livro) => {
+  const index = carrinho.value.itens.findIndex((item) => item.id === livro.id)
+  if (index === -1) {
+    const novoItem = {
+      ...livro,
+      quantidade: 1,
+      total: livro.price
+    }
+    carrinho.value.itens.push(novoItem)
+  } else {
+    const itemExistente = carrinho.value.itens[index]
+    itemExistente.quantidade++
+    itemExistente.total += livro.price
+  }
+  carrinho.value.total += livro.price
+}
+
+const removerItemCarrinho = (item) => {
+  const index = carrinho.value.itens.findIndex((i) => i.id === item.id)
+  carrinho.value.total -= item.total
+  carrinho.value.itens.splice(index, 1)
+}
+
+const atualizarQuantidadeItem = (item) => {
   carrinho.value.total -= item.total
   item.total = item.price * item.quantidade
   carrinho.value.total += item.total
 }
 
-function removerItemCarrinho(item){
-  const index = carrinho.value.itens.findIndex((i) => i.id === item.id)
-  carrinho.value.total -= item.total
-  carrinho.value.itens.splice(index,1)
-}
-function adicionarAoCarrinho(livro) {
-  const index = carrinho.value.itens.findIndex((item) => item.id === livro.id)
-  if (index === -1){
-    carrinho.value.itens.push({
-    ...livro,
-    quantidade: 1,
-    total: livro.price
-  })
-  carrinho.value.total += livro.price
-} else {
-  carrinho.value.itens[index].quantidade++
-  carrinho.value.itens[index].total += livro.price
-  carrinho.value.total += livro.price
-}
-}
-function formatarPreco(preco) {
+const formatarPreco = (preco) => {
   return 'R$ ' + preco.toFixed(2).replace('.', ',')
 }
+
+const mostrarFiltros = ref(false)
+
+const alternarFiltros = () => {
+  mostrarFiltros.value = !mostrarFiltros.value
+}
+
+const categorias = ref({
+  fantasia: false,
+  violencia: false,
+  infantojuvenil: false,
+  medieval: false,
+})
+
+const livrosFiltrados = computed(() => {
+  return livros.filter((livro) => {
+    if (
+      (categorias.value.fantasia && livro.class === "fantasia") ||
+      (categorias.value.violencia && livro.class === "violencia") ||
+      (categorias.value.infantojuvenil && livro.class === "infantojuvenil") ||
+      (categorias.value.medieval && livro.class === "medieval")
+    ) {
+      return true;
+    }
+    return false;
+  });
+});
+
+const aplicarFiltro = () => {
+  mostrarFiltros.value = false;
+}
+
 </script>
 
 <template>
-  <h1>Minha livraria</h1>
-  <div class="container-geral">
+  <main>
+    <div class="header-container">
+      <div class="header-bar"></div>
+      <h1 class="titulo-pagina">Minha Livraria</h1>
+      <button class="filtrar-button" @click="alternarFiltros">Filtrar</button>
+      <div v-if="mostrarFiltros" class="filtros-container">
+        <div class="filtro">
+          <input type="checkbox" v-model="categorias.fantasia" />
+          <label class="filtros">Fantasia</label>
+        </div>
+        <div class="filtro">
+          <input type="checkbox" v-model="categorias.violencia" />
+          <label class="filtros">Violência</label>
+        </div>
+        <div class="filtro">
+          <input type="checkbox" v-model="categorias.infantojuvenil" />
+          <label class="filtros">Infantojuvenil</label>
+        </div>
+        <div class="filtro">
+          <input type="checkbox" v-model="categorias.medieval" />
+          <label class="filtros">Medieval</label>
+        </div>
+      </div>
+    </div>
+    <div class="container-geral">
     <div class="listagem-livros">
-      <div class="card-livro" v-for="livro in livros" :key="livro.id">
-        <div class="wrap-livro">
-          <img :src="livro.img" alt="Capa do livro" class="capa-livro" />
-        </div>
-        <p class="titulo-livro">{{ livro.title }}</p>
-        <p class="autor-livro">{{ livro.author }}</p>
-        <p class="preco-livro">{{ formatarPreco(livro.price) }}</p>
-        <button @click="adicionarAoCarrinho(livro)">Adicionar ao carrinho</button>
-      </div>
-    </div>
-    <div class="carrinho">
-      <h2>Meu carrinho</h2>
-      <p v-if="carrinho.itens.length === 0">Seu carrinho está vazio</p>
-      <div v-else>
-        <div class="item-carrinho" v-for="(item, index) in carrinho.itens" :key="index">
-          <div class="info-livro">
-            <div class="imagem-livro">
-              <img :src="item.img" class="icon-capa-livro" />
-            </div>
-            <div class="detalhes-livro">
-              <div>
-                <p>{{ item.title }}</p>
-                <p class="info-livro-preco">{{ formatarPreco(item.price) }}/un</p>
-              </div>
-              <div>
-                <p>
-                  quantidade:
-  <input 
-    type="number"
-    v-model="item.quantidade"
-    @change="atualizaQuantidadeItem(item)" 
-    min="1"
-  >
-                </p>
-              </div>
-            </div>
+      <div class="card-livro" v-for="livro in livrosFiltrados" :key="livro.id">
+          <div class="wrap-livro">
+            <img :src="livro.img" alt="Capa do livro" class="capa-livro" />
           </div>
-          <button @click="removerItemCarrinho(item)">Remover</button>
+          <p class="titulo-livro">{{ livro.title }}</p>
+          <p class="autor-livro">{{ livro.author }}</p>
+          <p class="preco-livro">{{ formatarPreco(livro.price) }}</p>
+          <button @click="adicionarAoCarrinho(livro)">Adicionar ao carrinho</button>
         </div>
       </div>
-      <p>Total: {{ formatarPreco(carrinho.total) }}</p>
+      <div class="carrinho">
+        <h2>Meu carrinho</h2>
+        <p v-if="carrinho.itens.length === 0">Seu carrinho está vazio</p>
+        <div v-else>
+          <div class="item-carrinho" v-for="(item, index) in carrinho.itens" :key="index">
+            <div class="info-livro">
+              <div class="imagem-livro">
+                <img :src="item.img" class="icon-capa-livro" />
+              </div>
+              <div class="detalhes-livro">
+                <div>
+                  <p>{{ item.title }}</p>
+                  <p class="info-livro-preco">{{ formatarPreco(item.price) }}/un</p>
+                </div>
+                <div>
+                  <label for="quantidade">Quantidade:</label>
+                  <input
+                    type="number"
+                    v-model="item.quantidade"
+                    @input="atualizarQuantidadeItem(item)"
+                    min="1"
+                  />
+                </div>
+              </div>
+            </div>
+            <button @click="removerItemCarrinho(item)">&#128465;</button>
+          </div>
+        </div>
+        <p>Total: {{ formatarPreco(carrinho.total) }}</p>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <style scoped>
+
+.filtrar-button {
+  background-color: #73ac31;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+  margin-left: 10px;
+}
+
+.filtros-container {
+  margin-top: 10px;
+  border: 1px solid #73ac31;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.filtro {
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+}
+
+.filtros {
+  color: black;
+}
+.header-container {
+  position: relative;
+}
+.header-bar {
+  background-color: #73ac31;
+  height: 100px;
+  width: 100%;
+  
+}
+
+.titulo-pagina {
+  text-align: center;
+  color: #184a4a;
+  margin: 20px auto 0;
+  font-size: 28px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+
 .info-livro {
   display: flex;
   margin-bottom: 10px;
 }
+
 .detalhes-livro {
   display: flex;
   flex-direction: column;
   width: 100%;
 }
+
 .detalhes-livro p {
   margin: 0;
 }
+
 .detalhes-livro div {
   display: flex;
   justify-content: space-between;
   width: 100%;
 }
 
-.detalhes-livro input[type='number']{
+.detalhes-livro input[type="number"] {
   width: 50px;
   text-align: center;
   border: none;
@@ -114,12 +224,12 @@ function formatarPreco(preco) {
   margin-left: 10px;
 }
 
-.detalhes-livro button{
+.detalhes-livro button {
   background-color: transparent;
-  border:none ;
+  border: none;
   cursor: pointer;
   font-size: 1.5rem;
-  color:black;
+  color: black;
   padding: 0;
   margin: 0;
 }
@@ -127,48 +237,86 @@ function formatarPreco(preco) {
 .info-livro-preco {
   margin-left: auto;
 }
+
 .icon-capa-livro {
   width: 30px;
   margin-right: 10px;
 }
+
 .container-geral {
-  /* display: flex;
-  justify-content: space-between; */
   display: grid;
   grid-template-columns: 3fr 1fr;
+  gap: 20px;
 }
+
 .carrinho {
-  /* min-width: 20%; */
-}
-.listagem-livros {
-  display: flex;
-  flex-wrap: wrap;
-}
-.card-livro {
-  margin: 5px 10px;
-  padding: 10px;
-  background-color: beige;
+  background-color: #83eec5;
+  padding: 20px;
   border-radius: 10px;
-  width: 180px;
 }
+
+.listagem-livros {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  gap: 20px;
+}
+
+.card-livro {
+  padding: 10px;
+  background-color: #62d5b4;
+  border-radius: 10px;
+  width: 100%;
+}
+
 .wrap-livro {
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: white;
   border-radius: 10px;
-  width: 180px;
+  width: 100%;
   height: 270px;
 }
+
 .capa-livro {
   width: 90%;
   max-height: 100%;
 }
+
 .card-livro p {
   margin: 0;
 }
+
 .card-livro .titulo-livro {
   font-weight: bold;
   margin-bottom: 5px;
+}
+
+h1 {
+  color: #73ac31;
+}
+
+h2 {
+  color: #73ac31;
+  margin-bottom: 10px;
+}
+
+button {
+  background-color: #73ac31;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 768px) {
+  .container-geral {
+    grid-template-columns: 1fr;
+  }
+
+  .listagem-livros {
+    grid-template-columns:1fr;
+  }
 }
 </style>
--- src/_data/livros.js
+++ src/_data/livros.js
@@ -1,6 +1,7 @@
 export const livros = [
   {
     id: 1,
+    class: 'violencia',
     title: 'O Poderoso Chefão',
     author: 'Mario Puzo',
     price: 49.9,
@@ -8,6 +9,8 @@ export const livros = [
   },
   {
     id: 2,
+    class: 'violencia',
+    class: 'fantasia',
     title: 'O Senhor dos Anéis - Box completo',
     author: 'J. R. R. Tolkien',
     price: 129.9,
@@ -16,6 +19,8 @@ export const livros = [
   {
     id: 3,
     title: 'O Pequeno Príncipe',
+    class: 'fantasia',
+    id: 'infantojuvenil',
     author: 'Antoine de Saint-Exupéry',
     price: 19.9,
     img: 'https://m.media-amazon.com/images/I/41afCn3PQUL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
@@ -23,6 +28,10 @@ export const livros = [
   {
     id: 4,
     title: 'Dom Quixote',
+    class: 'fantasia',
+    class: 'medieval',
+    class: 'infantojuvenil',
+    id: 'infantojuvenil',
     author: 'Miguel de Cervantes',
     price: 39.9,
     img: 'https://m.media-amazon.com/images/I/513uHO8IDlL._SX359_BO1,204,203,200_.jpg'
@@ -30,6 +39,7 @@ export const livros = [
   {
     id: 5,
     title: 'O Conde de Monte Cristo',
+    class: 'violencia',
     author: 'Alexandre Dumas',
     price: 69.9,
     img: 'https://m.media-amazon.com/images/I/81ZswN9PVPL._AC_UL320_.jpg'
@@ -37,6 +47,8 @@ export const livros = [
   {
     id: 6,
     title: 'O Hobbit',
+    class: 'violencia',
+    class: 'fantasia',
     author: 'J. R. R. Tolkien',
     price: 39.9,
     img: 'https://m.media-amazon.com/images/I/91M9xPIf10L._AC_UL320_.jpg'
